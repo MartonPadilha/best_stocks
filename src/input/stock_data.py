6e05b21da4cdb835c82eb0ab4e2de617917145df
MartonPadilha <martonpad@gmail.com>
2025-04-28 22:34:43 -0300

    fix headers

--- src/input/stock_data.py
+++ src/input/stock_data.py
@@ -8,8 +8,17 @@ import numpy as np
 import datetime
 from tqdm import tqdm
 
+# HEADER = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+# }
+
 HEADER = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://www.google.com/",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 URL_BASE = f'https://statusinvest.com.br'
 LIST_PATH = 'storage/tickers_list.txt'
